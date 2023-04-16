d7a9f62b71bb375d8344f4bae3469f6ebb9e279a
YexuanXiao <bizwen@nykz.org>
2023-04-16 13:12:31 +0800

    separate hpp to header and cpp

--- src/SettingsHelper.cpp
+++ src/SettingsHelper.cpp
@@ -1,43 +1,41 @@
 #include "pch.h"
 #include "SettingsHelper.h"
 
-using namespace winrt;
-
 namespace SettingsHelper {
 
 	namespace impl_ {
 		constexpr std::wstring_view Theme_Key = L"Theme";
-		Windows::Foundation::Collections::IPropertySet GetApplicationSettings() {
-			return Windows::Storage::ApplicationData::Current().LocalSettings().Values();
+		winrt::Windows::Foundation::Collections::IPropertySet GetApplicationSettings() {
+			return winrt::Windows::Storage::ApplicationData::Current().LocalSettings().Values();
 		}
-		void StoreTheme(Microsoft::UI::Xaml::ElementTheme theme) {
+		void StoreTheme(winrt::Microsoft::UI::Xaml::ElementTheme theme) {
 			auto localSettings{ impl_::GetApplicationSettings() };
 			auto value{ static_cast<int32_t>(theme) };
 			localSettings.Insert(impl_::Theme_Key.data(), winrt::box_value<int32_t>(value));
 		}
 	}
-	Microsoft::UI::Xaml::ElementTheme LoadTheme() {
+	winrt::Microsoft::UI::Xaml::ElementTheme LoadTheme() {
 		auto localSettings{ impl_::GetApplicationSettings() };
 		auto value{ winrt::unbox_value_or<int32_t>(localSettings.Lookup(impl_::Theme_Key.data()),0) };
-		return Microsoft::UI::Xaml::ElementTheme{value};
+		return winrt::Microsoft::UI::Xaml::ElementTheme{value};
 	}
 	// for settings page use
-	void SetTheme(Microsoft::UI::Xaml::XamlRoot& element, Microsoft::UI::Xaml::ElementTheme theme) {
-		element.Content().as<Microsoft::UI::Xaml::FrameworkElement>().RequestedTheme(theme);
+	void SetTheme(winrt::Microsoft::UI::Xaml::XamlRoot& element, winrt::Microsoft::UI::Xaml::ElementTheme theme) {
+		element.Content().as<winrt::Microsoft::UI::Xaml::FrameworkElement>().RequestedTheme(theme);
 		impl_::StoreTheme(theme);
 	}
 	// for init
-	void SetTheme(Microsoft::UI::Xaml::FrameworkElement& element, Microsoft::UI::Xaml::ElementTheme theme) {
+	void SetTheme(winrt::Microsoft::UI::Xaml::FrameworkElement& element, winrt::Microsoft::UI::Xaml::ElementTheme theme) {
 		element.RequestedTheme(theme);
 	}
 	// for other page
-	void SetTheme(Microsoft::UI::Xaml::XamlRoot& element) {
-		element.Content().as<Microsoft::UI::Xaml::FrameworkElement>().RequestedTheme(LoadTheme());
+	void SetTheme(winrt::Microsoft::UI::Xaml::XamlRoot& element) {
+		element.Content().as<winrt::Microsoft::UI::Xaml::FrameworkElement>().RequestedTheme(LoadTheme());
 	}
-	void SetTitleBarTheme(Microsoft::UI::Windowing::AppWindowTitleBar& titlebar, Microsoft::UI::Xaml::ElementTheme theme) {
-		if (theme == Microsoft::UI::Xaml::ElementTheme::Default) [[likely]] {
+	void SetTitleBarTheme(winrt::Microsoft::UI::Windowing::AppWindowTitleBar& titlebar, winrt::Microsoft::UI::Xaml::ElementTheme theme) {
+		if (theme == winrt::Microsoft::UI::Xaml::ElementTheme::Default) [[likely]] {
 			titlebar.BackgroundColor(nullptr);
-			titlebar.ButtonBackgroundColor(Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonBackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
 			titlebar.ButtonForegroundColor(nullptr);
 			titlebar.ButtonInactiveBackgroundColor(nullptr);
 			titlebar.ButtonInactiveForegroundColor(nullptr);
@@ -46,27 +44,27 @@ namespace SettingsHelper {
 			titlebar.ButtonPressedBackgroundColor(nullptr);
 			titlebar.ButtonPressedForegroundColor(nullptr);
 		}
-		else if (theme == Microsoft::UI::Xaml::ElementTheme::Dark) {
-			titlebar.BackgroundColor(Microsoft::UI::Colors::Transparent());
-			titlebar.ButtonBackgroundColor(Microsoft::UI::Colors::Transparent());
-			titlebar.ButtonForegroundColor(Microsoft::UI::Colors::White());
-			titlebar.ButtonInactiveBackgroundColor(Microsoft::UI::Colors::Transparent());
-			titlebar.ButtonInactiveForegroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 96, 96, 96));
-			titlebar.ButtonHoverBackgroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 45, 45, 45));
-			titlebar.ButtonHoverForegroundColor(Microsoft::UI::Colors::White());
-			titlebar.ButtonPressedBackgroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 41, 41, 41));
-			titlebar.ButtonPressedForegroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 167, 167, 167));
+		else if (theme == winrt::Microsoft::UI::Xaml::ElementTheme::Dark) {
+			titlebar.BackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonBackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonForegroundColor(winrt::Microsoft::UI::Colors::White());
+			titlebar.ButtonInactiveBackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonInactiveForegroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 96, 96, 96));
+			titlebar.ButtonHoverBackgroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 45, 45, 45));
+			titlebar.ButtonHoverForegroundColor(winrt::Microsoft::UI::Colors::White());
+			titlebar.ButtonPressedBackgroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 41, 41, 41));
+			titlebar.ButtonPressedForegroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 167, 167, 167));
 		}
-		else if (theme == Microsoft::UI::Xaml::ElementTheme::Light) {
-			titlebar.BackgroundColor(Microsoft::UI::Colors::Transparent());
-			titlebar.ButtonBackgroundColor(Microsoft::UI::Colors::Transparent());
-			titlebar.ButtonForegroundColor(Microsoft::UI::Colors::Black());
-			titlebar.ButtonInactiveBackgroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 243, 243, 243));
-			titlebar.ButtonInactiveForegroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 144, 144, 144));
-			titlebar.ButtonHoverBackgroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 219, 219, 219));
-			titlebar.ButtonHoverForegroundColor(Microsoft::UI::Colors::Black());
-			titlebar.ButtonPressedBackgroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 194, 194, 194));
-			titlebar.ButtonPressedForegroundColor(Microsoft::UI::ColorHelper::FromArgb(255, 95, 95, 95));
+		else if (theme == winrt::Microsoft::UI::Xaml::ElementTheme::Light) {
+			titlebar.BackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonBackgroundColor(winrt::Microsoft::UI::Colors::Transparent());
+			titlebar.ButtonForegroundColor(winrt::Microsoft::UI::Colors::Black());
+			titlebar.ButtonInactiveBackgroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 243, 243, 243));
+			titlebar.ButtonInactiveForegroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 144, 144, 144));
+			titlebar.ButtonHoverBackgroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 219, 219, 219));
+			titlebar.ButtonHoverForegroundColor(winrt::Microsoft::UI::Colors::Black());
+			titlebar.ButtonPressedBackgroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 194, 194, 194));
+			titlebar.ButtonPressedForegroundColor(winrt::Microsoft::UI::ColorHelper::FromArgb(255, 95, 95, 95));
 		}
 	}
 }
---
+++ src/Win32Helper.cpp
@@ -0,0 +1,72 @@
+#include "pch.h"
+#include "Win32Helper.h"
+
+namespace Win32Helper {
+    /// <summary>
+    /// Make app only has one instance
+    /// call at entry point
+    /// </summary>
+    /// <param name="appname">size lesser than 16, zero ending</param>
+    /// <param name="exitcode">exit code</param>
+    void DisableMultiInstanceEntry(std::wstring_view const appname, UINT const exitcode) {
+        using namespace std::literals;
+        auto const max_size{ 16 };
+        assert(appname.size() < max_size);
+        // use user's temp folder pathname + appname as part of mutex name
+        // name buffer, initialized to be zero
+        auto name{ std::to_array<wchar_t, (MAX_PATH + 2) + max_size>({}) };
+        // get tmp folder path, not guaranteed availablity
+        auto length{ ::GetTempPathW(static_cast<DWORD>(name.size()), &name[0]) };
+        // append appname to path
+        std::memcpy(&name[length], appname.data(), appname.size() * sizeof(wchar_t));
+        // mutex name can't contain '\'
+        std::replace(&name[0], &name[length], '\\', '/');
+        // if mutex is held by another instance, NO NEED TO USE GetLastError
+        if (!::CreateMutexExW(nullptr, &name[0], CREATE_MUTEX_INITIAL_OWNER, NULL)) [[likely]] {
+            // enumerate all direct child windows of desktop
+            for (auto pre{ ::FindWindowExW(nullptr, nullptr, nullptr, nullptr) };
+                // return null if at the end
+                pre != nullptr;
+                // pass the pre as the 2nd argument to get next handle
+                pre = ::FindWindowExW(nullptr, pre, nullptr, nullptr)) [[likely]]
+            {
+                // check if window_ has the specified property
+                // set in MainWindow constructor
+                if (::GetPropW(pre, appname.data())) [[unlikely]] {
+                    // show and restore window_
+                    ::ShowWindow(pre, SW_RESTORE);
+                    // activate, set foreground and get forcus
+                    ::SetForegroundWindow(pre);
+                }
+            }
+            ::ExitProcess(exitcode);
+        }
+    }
+
+    /// warp ::GetDpiForMonitor
+    UINT GetDpiXForMonitor(HMONITOR hmonitor) {
+        auto dpiX{ UINT{} };
+        auto dpiY{ UINT{} };
+#pragma comment(lib, "Shcore.lib")
+        ::GetDpiForMonitor(hmonitor, MDT_DEFAULT, &dpiX, &dpiY);
+        return dpiX;
+    }
+    HWND GetHandleFromWindow(winrt::Microsoft::UI::Xaml::Window const& window) {
+        auto hWnd{ HWND{} };
+        window.as<::IWindowNative>()->get_WindowHandle(&hWnd);
+        return hWnd;
+    }
+    winrt::Microsoft::UI::WindowId GetWindowIdFromWindow(winrt::Microsoft::UI::Xaml::Window const& window) {
+        return winrt::Microsoft::UI::GetWindowIdFromWindow(GetHandleFromWindow(window));
+    }
+    /// <summary>
+    /// Make app only has one instance
+    /// call at window_ initialized
+    /// </summary>
+    /// <param name="appname">size lesser than 16</param>
+    /// <param name="handle">handle of window_</param>
+    void DisableMultiInstanceWindow(winrt::Microsoft::UI::Xaml::Window const& window, std::wstring_view const appname) {
+        auto handle{ GetHandleFromWindow(window) };
+        ::SetPropW(handle, appname.data(), handle);
+    }
+}
